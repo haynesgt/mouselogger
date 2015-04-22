de5d0c7cbed86fdceeccebc7dfd4b5eb493fb723
Gavin Haynes <haynes.gt@gmail.com>
2015-04-22 13:53:10 -0700

    Fix

--- replaymouse.c
+++ replaymouse.c
@@ -7,13 +7,17 @@
 #include <sys/time.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 void sleepd(double seconds)
 {
 	struct timespec x, y;
-	x.tv_sec = 0;
-	x.tv_nsec = floor(seconds * 1e9);
-	nanosleep(&x, &y);
+	x.tv_sec = floor(seconds);
+	x.tv_nsec = (seconds-x.tv_sec)*1e9;
+	while (x.tv_nsec + x.tv_sec > 0) {
+		nanosleep(&x, &y);
+		x = y;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -29,10 +33,13 @@ int main(int argc, char *argv[])
 	c = EOF+1;
 	while (c != EOF) {
 		if (scanf("%f: ", &time) == EOF) break;
-		if (c = getc(stdin) == EOF) break;
+		c = getc(stdin);
+		if (c == EOF) break;
 		if (scanf("%d (%d, %d)", &i, &mx, &my) == EOF) break;
-		printf ("%.20f: %c%d (%d, %d)\n", time, c, i, mx, my);
-		if (t != -1) sleepd((time - t)/2.0);
+		if (t != -1) {
+			sleepd((time - t)/1.0);
+			printf("Sleeping %f\n", (time - t)/1.0);
+		}
 		t = time;
 		XTestFakeMotionEvent(display, -1, mx, my, 0);
 		if (c == '+') {
@@ -41,6 +48,7 @@ int main(int argc, char *argv[])
 			XTestFakeButtonEvent(display, 1, False, CurrentTime);
 		}
 		XFlush(display);
+		printf ("%.20f: %c%d (%d, %d)\n", time, c, i, mx, my);
 		c = getc(stdin);
 	}
 	return 0;
